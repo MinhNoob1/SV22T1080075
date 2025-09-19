af874b4177f878edd775b2627567779ef3cc13d8
LAB506 <LAB506@LAB506-5>
2025-09-19 10:07:51 +0700

    Add project files.

---
+++ SV22T1080075.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SV22T1080075.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
