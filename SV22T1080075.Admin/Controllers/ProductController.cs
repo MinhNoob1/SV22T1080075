c9fc5b3f4dcab7bf1389c20589e2fffc282b2d10
lab <lab@LAB505-05>
2025-10-03 10:46:22 +0700

    3-10 (add image)

--- SV22T1080075.Admin/Controllers/ProductController.cs
+++ SV22T1080075.Admin/Controllers/ProductController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SV22T1080075.Admin.Controllers
 {
@@ -8,13 +8,39 @@ namespace SV22T1080075.Admin.Controllers
         {
             return View();
         }
-        public IActionResult Attribute()
+        public IActionResult Attribute(int id = 0, string method = "", int photoId = 0)
         {
-            return View();
+            switch (method)
+            {
+                case "add":
+                    ViewBag.Title = "Bổ sung thuộc tính cho mặt hàng";
+                    return View();
+                case "edit":
+                    ViewBag.Title = "Cập nhật thuộc tính mặt hàng";
+                    return View();
+                case "delete":
+                    //TODO: Xóa xong về lại trang
+                    return RedirectToAction("Edit", new { id = id });
+                default:
+                    return RedirectToAction("Index");
+            }
         }
-        public IActionResult Photo()
+        public IActionResult Photo(int id = 0, string method = "", int photoId = 0)
         {
-            return View();
+            switch (method)
+            {
+                case "add":
+                    ViewBag.Title = "Bổ sung ảnh cho mặt hàng";
+                    return View();
+                case "edit":
+                    ViewBag.Title = "Cập nhật hình ảnh mặt hàng";
+                    return View();
+                case "delete":
+                    //TODO: Xóa xong về lại trang
+                    return RedirectToAction("Edit", new {id = id});
+                default:
+                    return RedirectToAction("Index");
+            }
         }
         public IActionResult Edit(int id = 0)
         {
